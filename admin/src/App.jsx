071744d07ad2b66257d725e7a0f14c26a0ce9b99
adityasakhale <117585278+adityasakhale@users.noreply.github.com>
2025-01-29 11:07:19 +0530

    first commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Login from './components/Login'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
-import {Routes,Route} from 'react-router-dom'
+import Add from './pages/Add'
 import List from './pages/List'
 import Orders from './pages/Orders'
-import Add from './pages/add'
-import Login from './components/Login'
-import { ToastContainer } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
 
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL
